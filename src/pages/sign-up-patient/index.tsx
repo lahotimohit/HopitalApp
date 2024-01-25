d525f0a06a51d056ca2d17e19edf391bff0476c4
Mohit Lahoti <engtechno25@gmail.com>
2024-01-25 16:30:30 +0530

    Adding register user lottie animation

--- src/pages/sign-up-patient/index.tsx
+++ src/pages/sign-up-patient/index.tsx
@@ -1,12 +1,14 @@
-"use client"
+"use client";
 
 import React from "react";
 import Link from "next/link";
 import { Input } from "~/components/ui/input";
 import { Button } from "~/components/ui/button";
+import { useLottie } from "lottie-react";
+import regsiterAni from "../../../public/animation/register.json";
 import { MdLock } from "react-icons/md";
-import { CiUser } from 'react-icons/ci';
-import { useForm, SubmitHandler } from 'react-hook-form';
+import { CiUser } from "react-icons/ci";
+import { useForm, SubmitHandler } from "react-hook-form";
 import {
   Card,
   CardContent,
@@ -14,34 +16,54 @@ import {
   CardHeader,
   CardTitle,
 } from "~/components/ui/card";
-import { z } from 'zod';
+import { z } from "zod";
 
 const formSchema = z.object({
-  email: z.string().min(5, { message: "Min Length should be 5" }).max(50, { message: "Max Length should be 50" }), 
-  password: z.string().min(8, { message: "Min Length of password should be 8" }).max(25, { message: "Max Length should be 50" }),
-  confirmPassword: z.string()
-})
+  email: z
+    .string()
+    .min(5, { message: "Min Length should be 5" })
+    .max(50, { message: "Max Length should be 50" }),
+  password: z
+    .string()
+    .min(8, { message: "Min Length of password should be 8" })
+    .max(25, { message: "Max Length should be 50" }),
+  confirmPassword: z.string(),
+});
 
 type TFormSchema = z.infer<typeof formSchema>;
 
 function SignUp() {
   const {
-    handleSubmit, 
+    handleSubmit,
     formState: { errors },
-    register, 
-  } = useForm<TFormSchema>({
-    
-  });
+    register,
+  } = useForm<TFormSchema>({});
 
-  const handleSignUp: SubmitHandler<TFormSchema> = ({ email, confirmPassword, password }) => {
+  const handleSignUp: SubmitHandler<TFormSchema> = ({
+    email,
+    confirmPassword,
+    password,
+  }) => {
     if (password !== confirmPassword) {
       return;
     }
 
     console.log(email, confirmPassword, password);
-  }
+  };
+
+  const style = {
+    height: 150,
+  };
+
+  const options = {
+    animationData: regsiterAni,
+    loop: false,
+  };
+
+  const { View } = useLottie(options, style);
+
   return (
-    <div className="flex items-center justify-center h-screen w-full">
+    <div className="flex h-screen w-full items-center justify-center">
       <Card className="max-w-96 shadow-gray-600">
         <CardHeader>
           <CardTitle>
@@ -64,31 +86,44 @@ function SignUp() {
               className="w-40"
             />
           </Avatar> */}
-          <CiUser className="w-32 h-32 my-4 rounded-full p-2 border-4 border-black" />
-          <div className="flex flex-col gap-1 w-full">
+          <>{View}</>
+          {/* <CiUser className="my-4 h-32 w-32 rounded-full border-4 border-black p-2" /> */}
+          <div className="flex w-full flex-col gap-1">
             <Input
               id="useremail"
               className="mx-auto p-5"
               placeholder="Email Address"
-              {...register("email", { required: {
-                value: true, 
-                message: "Email is required"
-              } })}
+              {...register("email", {
+                required: {
+                  value: true,
+                  message: "Email is required",
+                },
+              })}
             />
-            {errors && errors.email && <span className="text-red-500 text-sm">{errors.email.message}</span>}
+            {errors && errors.email && (
+              <span className="text-sm text-red-500">
+                {errors.email.message}
+              </span>
+            )}
           </div>
-          <div className="flex flex-col gap-1 w-full">
+          <div className="flex w-full flex-col gap-1">
             <Input
               id="userpass"
               type="password"
               className="mx-auto mt-3 p-5"
               placeholder="Password"
-              {...register("password", { required: {
-                message: "Password is Required",
-                value: true
-              } })}
+              {...register("password", {
+                required: {
+                  message: "Password is Required",
+                  value: true,
+                },
+              })}
             />
-            {errors && errors.password && <span className="text-red-500 text-sm">{errors.password.message}</span>}
+            {errors && errors.password && (
+              <span className="text-sm text-red-500">
+                {errors.password.message}
+              </span>
+            )}
           </div>
           <Input
             id="userpass"
@@ -98,7 +133,10 @@ function SignUp() {
             {...register("confirmPassword", { required: true })}
           />
           {/* todo */}
-          <Button className="mt-5 w-4/5 rounded-full bg-blue-600 hover:bg-blue-800" onClick={handleSubmit(handleSignUp)}>
+          <Button
+            className="mt-5 w-4/5 rounded-full bg-blue-600 hover:bg-blue-800"
+            onClick={handleSubmit(handleSignUp)}
+          >
             <MdLock className="mr-2 h-4 w-4" /> Create Account
           </Button>
           <p className="p-4 font-sans text-xs italic">
